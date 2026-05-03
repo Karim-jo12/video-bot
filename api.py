2dda2971365cd08599db7440f1fa376397cf1122
unknown <karim.alhourani2011@gmail.com>
2026-05-03 20:47:08 +0300

    fix render port

--- api.py
+++ api.py
@@ -1,43 +1,28 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
-import threading
-import time
+import os
 
 app = Flask(__name__)
-CORS(app)
-
-status = {
-    "running": False,
-    "progress": 0,
-    "message": "Idle"
-}
 
 @app.route("/")
 def home():
-    return {"message": "API running"}
+    return "API is running"
 
 @app.route("/status")
-def get_status():
-    return jsonify(status)
+def status():
+    return jsonify({
+        "message": "Done",
+        "progress": 100,
+        "running": False
+    })
 
-def fake_task():
-    status["running"] = True
-    status["progress"] = 0
-    status["message"] = "Starting..."
-
-    for i in range(101):
-        time.sleep(0.05)
-        status["progress"] = i
-        status["message"] = f"Downloading {i}%"
-
-    status["running"] = False
-    status["message"] = "Done"
-
-@app.route("/start")
-def start():
-    if not status["running"]:
-        threading.Thread(target=fake_task).start()
-    return {"message": "Started"}
+@app.route("/progress")
+def progress():
+    return jsonify({
+        "message": "Idle",
+        "progress": 0,
+        "running": False
+    })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
